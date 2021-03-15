e6666d8e3839fc215b5ee3865e61be46d1454ba1
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-03-15 20:31:13 +1100

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on("ready", () => {
     client.guilds.forEach((guild) => {
         console.log(" - " + guild.name);
     });
-  scheduleWarning('09:27', function() { client.channels.get("699967983136800890").send("hi"); });
+  scheduleWarning('09:32', function() { client.channels.get("699967983136800890").send("hi"); });
 });
 
 client.on("message", async message => {
