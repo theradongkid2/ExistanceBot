fddc918884809d8ba66c8cf735cef7b8d2543031
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-03-15 22:34:50 +1100

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function scheduleWarning(time, triggerThis){
 //Terminal User Interface
 client.on("ready", () => {
   console.log(`Existence Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Having an Existential Crisis`);
+  client.user.setActivity(`e!help - Having an Existential Crisis`);
   console.log("Servers:");
     client.guilds.forEach((guild) => {
         console.log(" - " + guild.name);
