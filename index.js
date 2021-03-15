5975967d3cc53a5e8f60417854acf2fefb75dbf5
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-03-15 20:46:00 +1100

    Update index.js

--- index.js
+++ index.js
@@ -90,16 +90,32 @@ client.on("message", async message => {
     
     google.load("language", "1");
 
-    function initialize(a) {
-        var content = document.getElementById('content');
-        content.innerHTML = '';
-        google.language.translate(a, 'en', 'es', function(result) {
-            if (result.translation) {
-                message.channel.send(result.translation);
-            }
-        });
+    function pigLatin(str) {
+      // Convert string to lowercase
+      str = str.toLowerCase()
+      // Initialize array of vowels
+      const vowels = ["a", "e", "i", "o", "u"];
+      // Initialize vowel index to 0
+      let vowelIndex = 0;
+    
+      if (vowels.includes(str[0])) {
+        // If first letter is a vowel
+        return str + "way";
+      } else {
+        // If the first letter isn't a vowel i.e is a consonant
+        for (let char of str) {
+          // Loop through until the first vowel is found
+          if (vowels.includes(char)) {
+            // Store the index at which the first vowel exists
+            vowelIndex = str.indexOf(char);
+            break;
+          }
+        }
+        // Compose final string
+        return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+      }
     }
-    google.setOnLoadCallback(initialize(messageContent));
+    pigLatin(messageContent)
   }
 })
 
